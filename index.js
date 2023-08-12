578c1b3fbfc948aad5dba922b5da4171544cc6a5
Dylen <dylenbelanger2004@gmail.com>
2023-08-12 16:37:29 -0400

    Uncommented test cases

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import knightMoves from "./knightMoves.js";
 
 // TESTS
-// knightMoves([0, 0], [1, 2])
-// knightMoves([0, 0], [3, 3])
-// knightMoves([3, 3], [0, 0])
-// knightMoves([0, 0], [7, 7])
-// knightMoves([3, 6], [1, 4])
+knightMoves([0, 0], [1, 2])
+knightMoves([0, 0], [3, 3])
+knightMoves([3, 3], [0, 0])
+knightMoves([0, 0], [7, 7])
+knightMoves([3, 6], [1, 4])
 knightMoves([0, 1], [7, 5])
